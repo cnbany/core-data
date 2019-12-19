ba39e096ed657e672d5c8ad6462c61f9b6d2e670
cnbany <cnbany@163.com>
2019-12-19 21:54:49 +0800

    es

--- test/elastic-test.js
+++ test/elastic-test.js
@@ -1,16 +1,8 @@
 require("should")
-const elastic = require("../lib/elastic")
+// const elastic = require("../lib/elastic")
 
 describe('elastic', function () {
 
-    const es = new elastic("index-test")
-
-    context('#s2t()', function () {
-        it(' 繁体字 => 繁體字 ', function () {
-            chinese.s2t("繁体字").should.equal("繁體字")
-        });
-    });
-
-
+ 
 
 });
